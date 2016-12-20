ca4544215f7e6754c9497e492a821702cd92c1c7
RedColinar <1126525692@qq.com>
2016-12-20 23:23:21 +0800

    读取魔数 和版本号

--- src/jvmgo/ch03/classfile/class_file.go
+++ src/jvmgo/ch03/classfile/class_file.go
@@ -33,20 +33,50 @@ func Parse(classData []byte) (cf *ClassFile,err error){
 	cf.read(cr)
 	return
 }
-//read方法依次调用其他方法解析class文件
+//read方法依次调用其他方法解析class文件，注意是依次！  
 func (self *ClassFile) read(reader *ClassReader){
+	self.readAndCheckMagic(reader)
+	self.readAndCheckVersion(reader)
+	//self.constantPool = readConstantPool(reader)
 
 }
-
+//魔数，u43很多文件格式都会规定满足该格式的文件必须以固定字节开头
 func (self *ClassFile) readAndCheckMagic(reader *ClassReader){
-
+	magic := reader.readUint32()
+	if magic  != 0xCAFEBABE {
+		//调用panic()方法终止程序执行
+		panic("java.lang.ClassFormatError: magic!")
+	}
 }
+//魔数之后是class文件的次版本号和主版本号，都是u2类型的
 func (self *ClassFile) readAndCheckVersion(reader *ClassReader){
+	//次版本号
+	self.minorVersion = reader.readUint16()
+	//主版本号
+	self.majorVersion = reader.readUint16()
+	switch self.majorVersion{
+	case 45:
+		return
+	case 46,47,48,49,50,51,52:
+		if self.minorVersion == 0 {
+			return
+		}
+	}
+	panic("java.lang.UnsupportedClassVersionError!")
+}
+func (self *ClassFile) ClassName() string{
 
 }
-func (self *ClassFile) MinorVersion() uint16{
+func (self *ClassFile) SuperClassName() string{
 
+}
+func (self *ClassFile) InterfaceName() []string{
+
+}
+//下面的方法是Getter方法，把结构体的字段暴露给其他包使用
+func (self *ClassFile) MinorVersion() uint16{
+	return self.minorVersion
 }
 func (self *ClassFile) MajorVersion() uint16{
-
+	return self.majorVersion
 }
--- src/jvmgo/ch03/classfile/class_reader.go
+++ src/jvmgo/ch03/classfile/class_reader.go
@@ -1,23 +1,51 @@
 package classfile
 
 import "encoding/binary"
-
+//定义结构体用来读取数据
 type ClassReader struct{
 	data []byte
 }
 
 func (self *ClassReader) readUint8() uint8{
-
+	val := self.data[0]
+	self.data = self.data[1:]
+	return val
 }
-func (self *ClassReader) readUint16() uint16{
+/*	
+    51	func (littleEndian) Uint16(b []byte) uint16 {
+    52		_ = b[1] // bounds check hint to compiler; see golang.org/issue/14808
+    53		return uint16(b[0]) | uint16(b[1])<<8
+    54	}
 
+*/
+func (self *ClassReader) readUint16() uint16{
+	//标准库encoding/binary包定义了一个变量BigEndian，可以从[]byte中解码多字节数据。
+	val := binary.BigEndian.Uint16(self.data)
+	self.data = self.data[2:]
+	return val
 }
 func (self *ClassReader) readUint32() uint32{
-
+	val := binary.BigEndian.Uint32(self.data)
+	self.data =  self.data[4:]
+	return val
 }
+//java虚拟机规范没有定义u8
 func (self *ClassReader) readUint64() uint64{
-
+	val := binary.BigEndian.Uint64(self.data)
+	self.data = self.data[8:]
+	return val
 }
 func (self *ClassReader) readUint16s() []uint16{
-	
+	n := self.readUint16()
+	s := make([]uint16,n)
+	for i:=range s {
+		s[i] =  self.readUint16()
+	}
+	return s
+}
+//用于读取指定数量的字节
+func (self *ClassReader) readBytes(n uint32) []byte{
+	bytes := self.data[:n]
+	self.data = self.data[n:]
+	return bytes
 }
