13860ca62111e3c376221b7521e0573e760ed1e8
RedColinar <1126525692@qq.com>
2016-12-28 22:40:42 +0800

    局部变量表

---
+++ src/jvmgo/ch04/rtda/local_vars.go
@@ -0,0 +1,55 @@
+package rtda
+
+import "math"
+//局部变量表
+type LocalVars []Slot
+
+func newLocalVars(maxLocals uint) LocalVars {
+	if maxLocals >0{
+		return make([]Slot,maxLocals)
+	}
+	return nil
+}
+//存取Slot中的整数
+func (self LocalVars) SetInt(index uint,val int32){
+	self[index].num = val
+}
+func (self LocalVars) GetInt(index uint) int32{
+	return self[index].num
+}
+//float变量先转成int类型，然后按int类型处理
+func (self LocalVars) SetFloat(index uint,val float32){
+	bits := math.Float32bits(val)
+	self[index].num = int32(bits)
+}
+func (self LocalVars) GetFloat(index uint) float32{
+	bits := uint32(self[index].num)
+	return math.Float32frombits(bits)
+}
+//long变量拆成两个int变量
+func (self LocalVars) SetLong(index uint,val int64) {
+	self[index].num = int32(val)
+	self[index+1].num = int32(val >> 32)
+}
+//int64对应java的long
+func (self LocalVars) GetLong(index uint) int64 {
+	low := uint32(self[index].num)
+	high :=  uint32(self[inde+1].num)
+	return int64(high) << 32 | int64(low)
+}
+//double变量可以先转成long型，在按照long类型处理
+func (self LocalVars) SetDouble(index uint,val  float64){
+	bits := math.FLoat64bits(val)
+	self.SetLong(index ,int64(bits))
+}
+func (self LocalVars) GetDouble(index uint) float64{
+	bits := uint64(self.GetLong(index))
+	return math.FLoat64frombits(bits)
+}
+//引用值
+func (self LocalVars) SetRef(index  uint,ref *Object){
+	self[index].ref = ref
+}
+func (self LocalVars) GetRef(index uint) *Object{
+	return self[index].ref
+}
