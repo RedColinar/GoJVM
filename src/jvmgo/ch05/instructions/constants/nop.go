ba7754aebbccaec9d7240885b25da439c9c12c2a
RedColinar <1126525692@qq.com>
2017-01-02 19:55:47 +0800

    const指令

--- src/jvmgo/ch05/instructions/constants/nop.go
+++ src/jvmgo/ch05/instructions/constants/nop.go
@@ -1,20 +1,7 @@
-package constants
-
-import "jvmgo/ch05/instructions/base"
 import "jvmgo/ch05/rtda"
 
-type ACONST_NULL struct { base.NoOperandsInstruction }
-type DCONST_0 struct { base.NoOperandsInstruction }
-type ICONST_M1 struct { base.NoOperandsInstruction }
-//aconst_null指令把null引用推入操作数栈顶
-func (self *ACONST_NULL) Execute(frame *rtda.Frame){
-	frame.OperandStack().PushRef(nil)
-}
-//dconst_0指令把double型0推入操作数栈顶
-func (self *DCONST_0) Execute(frame *rtda.Frame){
-	frame.OperandStack().PushDouble(0.0)
-}
-//iconst_m1把int型-1推入操作数栈顶
-func (self *ICONST_M1) Execute(frame *rtda.Frame){
-	frame.OperandStack().PushInt(-1)
-}
+type NOP struct { base.NoOperandsInstruction }
+
+func (self *NOP) Execute(frame *rtda.Frame){
+	
+} 
