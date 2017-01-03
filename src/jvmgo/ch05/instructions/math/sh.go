289eea0e067627e09a57f9c66c3ee58b62f1eb5e
RedColinar <1126525692@qq.com>
2017-01-03 23:54:55 +0800

    位移指令

--- src/jvmgo/ch05/instructions/math/sh.go
+++ src/jvmgo/ch05/instructions/math/sh.go
@@ -9,12 +9,31 @@ type IUSHR struct { base.NoOperandsInstruction }
 type LSHL struct { base.NoOperandsInstruction }
 type LSHR struct { base.NoOperandsInstruction }
 type LUSHR struct { base.NoOperandsInstruction }
-//左移指令
+//int左移指令
 func (self *ISHL) Execute(frame *rtda.Frame){
 	stack := frame.OperandStack()
 	v2 := stack.PopInt()
 	v1 := stack.PopInt()
 	s := uint32(v2) & 0x1f
 	result := v1 << s
+	stack.PushInt(result)
+}
+//long右移指令
+func (self *LSHR) Execute(frame *rtda.Frame){
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	//long类型有64位
+	v1 := stack.PopLong()
+	s := uint32(v2) & 0x3f
+	result := v1 >> s
+	stack.PushLong(result)   
+}
+
+func (self *IUSHR) Execute(frame *rtda.Frame){
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	s := uint32(v2) & 0x1f
+	result := int32(uint32(v1) >> s)
 	stack.PushInt(result)
 }
