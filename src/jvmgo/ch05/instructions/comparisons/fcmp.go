cce1ca94942def3f6ef656759523256b52150c5a
RedColinar <1126525692@qq.com>
2017-01-04 15:15:47 +0800

    比较指令

---
+++ src/jvmgo/ch05/instructions/comparisons/fcmp.go
@@ -0,0 +1,32 @@
+package comparisons
+
+import "jvmgo/ch05/instructions/base"
+import "jvmgo/ch05/rtda"
+
+type FCMPG struct { base.NoOperandsInstruction }
+type FCMPL struct { base.NoOperandsInstruction }
+//gFlag用来讨论两个float变量中至少有一个是NaN时，
+//fcmpg返回1，fcmpl返回-1
+func _fcmp(frame *rtda.Frame, gFlag bool){
+	stack := frame.OperandStack()
+	v2 := stack.PopFloat()
+	v1 := stack.PopFloat()
+	if v1 > v2 {
+		stack.PushInt(1)
+	}else if v1 == v2{
+		stack.PushInt(0)
+	}else if v1 < v2{
+		stack.PushInt(-1)
+	}else if gFlag{
+		stack.PushInt(1)
+	}else{
+		stack.PushInt(-1)
+	}
+}
+
+func (self *FCMPG) Execute(frame *rtda.Frame){
+	_fcmp(frame,true)
+}
+func (self *FCMPL) Execute(frame *rtda.Frame){
+	_fcmp(frame,false)
+}
--- src/jvmgo/ch05/instructions/math/sh.go
+++ src/jvmgo/ch05/instructions/math/sh.go
@@ -18,7 +18,15 @@ func (self *ISHL) Execute(frame *rtda.Frame){
 	result := v1 << s
 	stack.PushInt(result)
 }
-//long右移指令
+func (self *ISHR) Execute(frame *rtda.Frame){
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	s := uint32(v2) & 0x1f
+	result := v1 >> s
+	stack.PushInt(result)
+}
+//long右移指令，long有64位，
 func (self *LSHR) Execute(frame *rtda.Frame){
 	stack := frame.OperandStack()
 	v2 := stack.PopInt()
@@ -28,7 +36,16 @@ func (self *LSHR) Execute(frame *rtda.Frame){
 	result := v1 >> s
 	stack.PushLong(result)   
 }
-
+func (self *LSHL) Execute(frame *rtda.Frame){
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopLong()
+	s := uint32(v2) & 0x3f
+	result := v1 << s
+	satck.PushLong(result)
+}
+//只有无符号右移，go中没有java的>>>运算符，
+//需要先把v1转成无符号整数，位移操作之后，再转回有符号整数
 func (self *IUSHR) Execute(frame *rtda.Frame){
 	stack := frame.OperandStack()
 	v2 := stack.PopInt()
@@ -36,4 +53,13 @@ func (self *IUSHR) Execute(frame *rtda.Frame){
 	s := uint32(v2) & 0x1f
 	result := int32(uint32(v1) >> s)
 	stack.PushInt(result)
+}
+func (self *LUSHR) Execute(frame *rtda.Frame){
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopLong()
+	//0x3f是六个1
+	s := uint32(v2) & 0x3f
+	result := int32(uint32(v1) >> s)
+	stack.PushLong(result)
 }
