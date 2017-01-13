ec225fbfcb688e0886135fba2589cbc3ed2a6ca3
RedColinar <1126525692@qq.com>
2017-01-13 19:38:06 +0800

    新增关于类的指令

--- src/jvmgo/ch06/main.go
+++ src/jvmgo/ch06/main.go
@@ -2,8 +2,8 @@
 
 import  "fmt"
 import  "strings"
-import  "jvmgo/ch06/classfile"
 import  "jvmgo/ch06/classpath"
+import  "jvmgo/ch06/rtda/heap"
 
 
 func main() {
@@ -20,33 +20,14 @@ func main() {
 }
 func startJVM(cmd *Cmd) {
 	cp := classpath.Parse(cmd.XjreOption,cmd.cpOption)
-	className := strings.Replace(cmd.class,".","/",-1)
-	cf := loadClass(className, cp)
-	mainMethod := getMainMethod(cf)
+	classLoader := heap.NewClassLoader(cp)
+
+	className := strings.Replace(cmd.class, ".", "/", -1)
+	mainClass := classLoader.LoadClass(className)
+	mainMethod := mainClass.GetMainMethod()
 	if mainMethod != nil {
 		interpret(mainMethod)
-	}else{
-		fmt.Printf("Main method not found in class %s\n",cmd.class)
+	} else {
+		fmt.Printf("Main method not found in class %s\n", cmd.class)
 	}
-}
-//读取并解析class文件
-func loadClass(className string, cp *classpath.Classpath) *classfile.ClassFile{
-	classData, _, err := cp.ReadClass(className)
-	if err != nil {
-		panic(err)
-	}
-	cf,err := classfile.Parse(classData)
-	if err != nil {
-		panic(err)
-	}
-	return cf
-}
-//调用getMainMethod()函数查找类的main()方法
-func getMainMethod(cf *classfile.ClassFile) *classfile.MemberInfo{
-	for _, m := range cf.Methods(){
-		if m.Name() == "main" && m.Descriptor() == "([Ljava/lang/String;)V"{
-			return m
-		}
-	}
-	return nil
 }
--- src/jvmgo/ch06/rtda/heap/class.go
+++ src/jvmgo/ch06/rtda/heap/class.go
@@ -69,3 +69,16 @@ func (self *Class) getPackageName() string{
 	}
 	return ""
 }
+//
+func (self *Class) GetMainMethod() *Method {
+	return self.getStaticMethod("main", "([Ljava/lang/String;)V")
+}
+func (self *Class) getStaticMethod(name,descriptor string) *Method {
+	for _, method := range self.methods {
+		if method.IsStatic() &&
+			method.name == name && method.descriptor == descriptor{
+				return method
+			}
+	}
+	return nil
+}
