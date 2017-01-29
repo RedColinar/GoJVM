25d0d87e1b3fec4b313c6c172ad14031fb9f3720
Colinar <1126525692@qq.com>
2017-01-29 15:45:21 +0800

    Update ldc.go
    完善ldc指令

--- src/jvmgo/ch08/instructions/constants/ldc.go
+++ src/jvmgo/ch08/instructions/constants/ldc.go
@@ -2,6 +2,7 @@ package constants
 
 import "jvmgo/ch08/instructions/base"
 import "jvmgo/ch08/rtda"
+import "jvmgo/ch08/rtda/heap"
 
 type LDC struct{ base.Index8Instruction }
 type LDC_W struct{ base.Index16Instruction }
@@ -14,15 +15,20 @@ func (self *LDC) Execute(frame *rtda.Frame){
 func (self *LDC_W) Execute(frame *rtda.Frame){
 	_ldc(frame, self.Index)
 }
+
 func _ldc(frame *rtda.Frame, index uint) {
 	stack := frame.OperandStack()
-	cp := frame.Method().Class().ConstantPool()
-	c := cp.GetConstant(index)
-
+	class := frame.Method().Class()
+	c := class.ConstantPool().GetConstant(index)
+	
 	switch c.(type){
 	case int32: stack.PushInt(c.(int32))
 	case  float32: stack.PushFloat(c.(float32))
-	//case string:
+	case string: 
+		//如果_ldc试图从运行时常量池中加载字符串常亮，先通过常量拿到go字符串，然后把它转化成java字符串实例
+		internedStr := heap.JString(class.Loader(), c.(stirng))
+		//把引用推入栈顶
+		stack.PushRef(internedStr)
 	//cse *heap.ClassRef:
 	default:  panic("todo: ldc!")
 	}
