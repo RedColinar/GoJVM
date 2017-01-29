3a72aaffaa2776ab014695c737ef56d8a2087e8e
Colinar <1126525692@qq.com>
2017-01-29 16:42:38 +0800

    Update object.go

--- src/jvmgo/ch08/rtda/heap/object.go
+++ src/jvmgo/ch08/rtda/heap/object.go
@@ -21,6 +21,12 @@ func (self *Object) Class() *Class {
 func (self *Object) Fields() Slots {
 	return self.data.(Slots)
 }
+func (self *Object) GetRefVar(name, descriptor string) *Object{
+	field := self.class.getField(name, descriptor, false)
+	slots := self.data.(Slots)
+	return slots.GetRef(field.slotId)
+}
+
 func (self *Object) SetRefVar(name, descriptor string, ref *Object){
 	field := self.class.getField(name, descriptor, false)
 	slots := self.data.(Slots)
